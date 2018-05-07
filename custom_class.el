ca69ec9acf5c2067f737586bb8f197768b127c47
daniel garcia vaglio <degv364@gmail.com>
2018-05-07 17:19:06 -0600

    Add the emacs lisp file for the classes

---
+++ custom_class.el
@@ -0,0 +1,27 @@
+;; Nice orgmode bullets
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+;; Use Org-ref
+(require 'org-ref)
+
+;; Definition of the proyectoelectrico class for orgmode
+(add-to-list 'org-latex-classes
+             '("proyectoelectrico"
+               "\\documentclass{proyectoelectrico}"
+	       ("\\chapter{%s}" . "\\chapter*{%s}")
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+;; Definiton of the calss for IEEE transactions
+(add-to-list 'org-latex-classes
+             '("IEEEtran"
+               "\\documentclass[letterpaper, 10pt, conference]{IEEEtran}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
